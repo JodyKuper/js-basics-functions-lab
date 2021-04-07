9713e10628bf81c3306744c5bfc26f8457eb337d
Joel Cooper <jodycooper16@gmail.com>
2021-04-07 15:44:04 -0700

    commit tree 62e10c117bf77b67e10f9d31f92bd3c8e2bd74a6

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+if (distance>42) {
+    return distance -42 
+}
+else {
+    return 42-distance
+}
+}
+
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks)*264
+}
+
+function distanceTravelledInFeet(begin, end) {
+    if (begin < end){
+        return (end-begin)*264     
+    }else {
+        return (begin-end)*264
+    }      
+}
+
+function calculatesFarePrice(begin,end){
+    let feet = distanceTravelledInFeet(begin, end)
+    if (feet < 400){
+        return 0
+    }
+    else if (feet > 400 && feet < 2000)  {
+        return (feet-400)*.02
+    }
+
+    else if  (feet > 2001 && feet < 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+    
+}
+
+
